25793b68956f589d73f6f24b5a170f0810bde546
raushanGWL <raushan.kumar@goodworklabs.com>
2026-04-08 15:56:13 +0530

    final commit 1

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   return (
     <ModalProvider>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col overflow-x-hidden">
         <Header />
         <main id="main-content" role="main" className="flex-1">
           {children}
